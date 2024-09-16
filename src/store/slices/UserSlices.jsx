393419061e7c02b75922b1e237b9200eebbea2dc
inderjeet07 <inderjeetsingh07112000@gmail.com>
2024-09-16 16:47:32 +0530

    added the slice

---
+++ src/store/slices/UserSlices.jsx
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice=createSlice({
+    name:"users",
+    initialState:[],
+    reducers:{
+        addCartProducts(state,action){
+        },
+        removeCartProducts(state,action){
+
+        }
+    },
+});
+console.log(userSlice.actions);
+
+export default userSlice.reducer;
