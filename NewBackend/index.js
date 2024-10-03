9cf03ee197203f947f693b9b298adf0bcb2a0be1
inderjeet07 <inderjeetsingh07112000@gmail.com>
2024-10-03 16:46:04 +0530

    working on node backend

--- NewBackend/index.js
+++ NewBackend/index.js
@@ -3,22 +3,50 @@ const fs= require("fs");
 const url=require("url");
 const { log } = require("console");
 const express=require('express');
+const users=require('./MOCK_DATA.json')
 
 const app = express();
 
-app.get('/',(req,res)=>{
+app.use(express.urlencoded({extended:false}))
 
-return res.send("Hello This is Homepage")
 
-});
+// app.get('/users',(req,res)=>{
+//     return(
+//         res.json(users)
+//     )
+// });
+// app.get('/users/:id',(req,res)=>{
+//     console.log("request paramter_id",req.params.id)
+//     const current_user = users.find((obj)=>(obj.id === Number(req.params.id)))
+//     // console.log("current-user-details",current_user)
+//     return(
+//         res.json(current_user)
+//     )
+// })
+// .post('users/:id',(req,res)=>{
+//     return res.json({status:"pending"})
+// })
+// .patch('users/:id',(req,res)=>{
+//     return res.json({status:"pending"})
+// });
 
-app.get('/about',(req,res)=>{
-    return res.send(`This is the aboutpage and hey ${req.query.name}`)
-});
+// app.listen(8000,()=>console.log('server start'))
 
-app.listen(8001,()=>{
-    console.log("server is running");
-})
+
+
+// app.get('/',(req,res)=>{
+
+// return res.send("Hello This is Homepage")
+
+// });
+
+// app.get('/about',(req,res)=>{
+//     return res.send(`This is the aboutpage and hey ${req.query.name}`)
+// });
+
+// app.listen(8001,()=>{
+//     console.log("server is running");
+// })
 
 // const NewBackend=http.createServer((req,res)=>{
 
@@ -51,3 +79,81 @@ app.listen(8001,()=>{
 //     })
 // });
 // NewBackend.listen(8000,()=>console.log('server started'));
+
+app.get('/api/users',(req,res)=>{
+    return(res.json(users));
+});
+
+app.post('/api/users',(req,res)=>{
+    const body_response=req.body;
+    users.push({...body_response,id:users.length+1});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+
+        console.log("bodyy-----------",body_response)
+        res.json({status:"success"})
+    });
+   
+})
+
+app.get('/api/users/:id',(req,res)=>{
+
+    const current_user = users.find((obj)=>(obj.id === Number(req.params.id)));
+
+    console.log("current_user==========",current_user)
+
+    return(
+        res.json(current_user)
+    )
+
+})
+.patch('/api/users/:id',(req,res)=>{
+    const user_id = req.params.id;
+
+    console.log("userId",user_id);
+
+    var already_data = users.find((obj)=>obj.id === Number(user_id))
+
+    console.log("already_data", already_data);
+
+    already_data.email = req.body.email;
+    already_data.first_name = req.body.first_name;
+    already_data.last_name = req.body.last_name;
+
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        res.json({status:"Success"})
+
+    })
+
+})
+.delete('/api/users/:id',(req,res)=>{
+    const current_id = Number(req.params.id);
+
+    console.log("curreeenttttttt",typeof(req.params.id))
+
+    var indexOfCouseInJson = users.map(function(item) { return item.id; }).indexOf(current_id); //find the index of :id
+
+    // .indexOf(current_id);
+    console.log("indexOfCouseInJson",indexOfCouseInJson)
+
+    if(indexOfCouseInJson!==-1){
+
+        users.splice(indexOfCouseInJson,1);
+
+
+
+
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        // if (err) {
+        //     console.error("Error writing to file:", err);
+        //     return res.status(500).json({ error: "Failed to write to file." });
+        // }
+        res.json({ status: "success" });
+    })}else {
+        // User not found
+        res.status(404).json({ error: "User not found." });
+    }
+    // users.reduce((obj)=>({
+    //     if()
+    // }))
+})
+app.listen(8000,()=>console.log('server started'));
